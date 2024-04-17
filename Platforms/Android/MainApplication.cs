119429d6950a0d67f0f5036e6e987f222cd84612
kino <p@kinon.sakura.ne.jp>
2024-04-17 22:23:11 +0900

    GPS permission

--- Platforms/Android/MainApplication.cs
+++ Platforms/Android/MainApplication.cs
@@ -1,6 +1,13 @@
 ﻿using Android.App;
 using Android.Runtime;
 
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessBackgroundLocation)]
+[assembly: UsesFeature("android.hardware.location", Required = false)]
+[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+[assembly: UsesFeature("android.hardware.location.network", Required = false)]
+
 namespace mobiletelemetry;
 
 [Application]
